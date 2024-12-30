dbae5134fa27d3dd1ce131d424160d7256de0c93
shpark47 <shp04078@naver.com>
2024-12-30 17:47:30 +0900

    스프링부트버전

---
+++ src/main/java/kh/GiveHub/GiveHubApplication.java
@@ -0,0 +1,13 @@
+package kh.GiveHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveHubApplication.class, args);
+	}
+
+}
