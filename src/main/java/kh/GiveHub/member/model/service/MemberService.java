9c879045d2ef013ccb9fd8d3d1aef6116053c70b
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-01-06 14:08:08 +0900

    이메일중복 확인기능

--- src/main/java/kh/GiveHub/member/model/service/MemberService.java
+++ src/main/java/kh/GiveHub/member/model/service/MemberService.java
@@ -2,8 +2,17 @@ package kh.GiveHub.member.model.service;
 
 import org.springframework.stereotype.Service;
 
+import kh.GiveHub.member.model.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+	
+	private final MemberMapper mapper;
+	public int checkEmail(String email) {
+		return mapper.checkEmail(email);
+	}
 
 
 
