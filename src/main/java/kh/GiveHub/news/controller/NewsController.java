f5d43748df3e7149e35b5ebf770240cc6e2cd7fe
shpark47 <shp04078@naver.com>
2025-01-09 17:28:45 +0900

    newsList ~ newsManage 거의완성

--- src/main/java/kh/GiveHub/news/controller/NewsController.java
+++ src/main/java/kh/GiveHub/news/controller/NewsController.java
@@ -1,12 +1,34 @@
 package kh.GiveHub.news.controller;
 
+import kh.GiveHub.news.model.vo.News;
 import org.springframework.stereotype.Controller;
 
 import kh.GiveHub.news.model.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class NewsController {
 	private final NewsService nService;
+
+	@GetMapping("/admin/newslist")
+	public String newsList() {
+		return "admin/NewsList";
+	}
+
+	@GetMapping("/admin/newsmanage")
+	public String newsManage(@RequestParam("nNo") String nNo, Model model) {
+		News n = new News();
+		if (nNo.equals("0")){
+			model.addAttribute("n", n);
+			return "admin/NewsManage";
+		}else{
+			n = nService.selectNews(nNo);
+			model.addAttribute("n", n);
+			return "admin/NewsManage";
+		}
+	}
 }
--- src/main/java/kh/GiveHub/news/model/vo/News.java
+++ src/main/java/kh/GiveHub/news/model/vo/News.java
@@ -1,5 +1,20 @@
 package kh.GiveHub.news.model.vo;
 
-public class News {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class News {
+    private int newsNo;
+    private String newsTitle;
+    private String newsContent;
+    private Date newsCreateDate;
+    private String newsStatus;
+    private String memNo;
+    private String doNo;
 }
