d8930757f4e279ae5cb4180e40a1ec42958ebb9e
shpark47 <shp04078@naver.com>
2025-01-14 16:19:04 +0900

    member vo 수정, 관리자메인 수정

--- src/main/java/kh/GiveHub/member/model/service/MemberService.java
+++ src/main/java/kh/GiveHub/member/model/service/MemberService.java
@@ -1,9 +1,12 @@
 package kh.GiveHub.member.model.service;
 
+import kh.GiveHub.member.model.vo.Member;
 import org.springframework.stereotype.Service;
 
 import kh.GiveHub.member.model.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -15,4 +18,7 @@ public class MemberService {
 		return mapper.checkEmail(email);
 	}
 
+    public ArrayList<Member> selectMemberList() {
+		return mapper.selectMemberList();
+    }
 }
