56461ea4137b35fe9948cc39e8ed95e1c273ae97
user1 <user1@DESKTOP-BLK4KRB>
2025-01-14 22:05:58 +0900

    로그인 구현중

--- src/main/java/kh/GiveHub/member/controller/MemberController.java
+++ src/main/java/kh/GiveHub/member/controller/MemberController.java
@@ -3,22 +3,51 @@ package kh.GiveHub.member.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import kh.GiveHub.member.model.vo.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletResponse;
 import kh.GiveHub.member.model.service.MemberService;
+import kh.GiveHub.member.model.vo.Member;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-
+	
     private final MemberService mService;
+    
+    //로그인 화면 연결
+    @GetMapping("/member/login")
+    public String logIn() {
+    	return "/member/login";
+    }
+    
+    //로그인
+    @PostMapping("/member/login")
+    public void login(Member m) {
+//    	System.out.println("id : " + m.getMemId());
+//    	System.out.println("pwd : " + m.getMemPwd());
+    	
+    	Member loginUser = mService.login(m);
+    	System.out.println(loginUser);
+    }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @GetMapping("/admin/main")
     public String adminMain(Model model) {
         ArrayList<Member> list = mService.selectMemberList();
