d855a77ebaba5d2654c4e996f4bbcab5df359913
desertdevv <desertdevv@gmail.com>
2025-01-15 09:23:42 +0900

    Revert "Reao"

--- src/main/java/kh/GiveHub/donation/model/mapper/DonationMapper.java
+++ src/main/java/kh/GiveHub/donation/model/mapper/DonationMapper.java
@@ -1,14 +1,8 @@
 package kh.GiveHub.donation.model.mapper;
 
-import java.util.ArrayList;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import kh.GiveHub.donation.model.vo.Donation;
 
 @Mapper
 public interface DonationMapper {
-
-	ArrayList<Donation> searchBoard(String keyword);
 
 }
