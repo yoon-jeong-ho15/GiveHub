a1288363c318656d4c87d759543129af7ba6ca42
DESKTOP-JFGDFP4\user <desertdevv@gmail.com>
2025-01-16 08:59:00 +0900

    뭐가 다깨져있음

--- src/main/java/kh/GiveHub/donation/controller/DonationController.java
+++ src/main/java/kh/GiveHub/donation/controller/DonationController.java
@@ -2,14 +2,19 @@ package kh.GiveHub.donation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
+import kh.GiveHub.donation.model.vo.Donation;
 import kh.GiveHub.member.model.vo.Member;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class DonationController {
+	private final Donation dService;
+
 	
 	@GetMapping("/ongoingList")
 	public String ongoingList(HttpSession session) {
@@ -21,6 +26,20 @@ public class DonationController {
 	public String finishedList(HttpSession session) {
 		
 		return "templates/member/mydonation";
+	}
+	
+	@GetMapping("/donationlist")
+	public String donationlist() {
+		return "/page/donationlist";
+	}
+	
+	
+	@GetMapping("detailCategory")
+	public String detailCategory(@RequestParam ) {
+		
+		d.setDoCategory();
+
+		
 	}
 	 
 }
