aa31b66b9e604849389bb92f1007641af622cb61
DESKTOP-JFGDFP4\user <desertdevv@gmail.com>
2025-01-16 15:28:56 +0900

    Merge branch 'main' of https://github.com/shpark47/GiveHub.git into HEAD

--- src/main/java/kh/GiveHub/donation/controller/DonationController.java
+++ src/main/java/kh/GiveHub/donation/controller/DonationController.java
@@ -1,11 +1,14 @@
 package kh.GiveHub.donation.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
+import kh.GiveHub.donation.model.service.DonationService;
 import kh.GiveHub.donation.model.vo.Donation;
 import kh.GiveHub.member.model.vo.Member;
 import lombok.RequiredArgsConstructor;
@@ -13,19 +16,19 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class DonationController {
-	private final Donation dService;
+	private final DonationService dService;
 
 	
 	@GetMapping("/ongoingList")
 	public String ongoingList(HttpSession session) {
 		Member loginUser = (Member)session.getAttribute("loginUser");
-		return "templates/member/mydonation";
+		return "/member/mydonation";
 	}
 	
 	@GetMapping("/finishedList")
 	public String finishedList(HttpSession session) {
 		
-		return "templates/member/mydonation";
+		return "/member/mydonation";
 	}
 	
 	@GetMapping("/donationlist")
@@ -35,11 +38,10 @@ public class DonationController {
 	
 	
 	@GetMapping("detailCategory")
-	public String detailCategory(@RequestParam ) {
-		
-		d.setDoCategory();
+	public ArrayList<Donation> detailCategory(@RequestParam("doCategory") String category) {
+		ArrayList<Donation> list = dService.detailCategory(category);
 
-		
+		return list;
 	}
 	 
 }
--- src/main/java/kh/GiveHub/donation/model/service/DonationService.java
+++ src/main/java/kh/GiveHub/donation/model/service/DonationService.java
@@ -1,13 +1,23 @@
 package kh.GiveHub.donation.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import kh.GiveHub.donation.model.mapper.DonationMapper;
+import kh.GiveHub.donation.model.vo.Donation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 @RequiredArgsConstructor
+
 public class DonationService {
 	private final DonationMapper mapper;
+	
+	public ArrayList<Donation> detailCategory(String category) {
+		return mapper.detailCategory(category);
+	}
+	
+
 }
