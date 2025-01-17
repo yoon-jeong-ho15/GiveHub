23b1b1da6913d39b928603c172b8163981a8c414
DESKTOP-JFGDFP4\user <desertdevv@gmail.com>
2025-01-17 16:06:06 +0900

    250117

--- src/main/java/kh/GiveHub/donation/controller/DonationController.java
+++ src/main/java/kh/GiveHub/donation/controller/DonationController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
@@ -39,12 +40,18 @@ public class DonationController {
 	}
 	
 	@GetMapping("/page/donationlist")
-	public ArrayList<Donation> donationlist(@ModelAttribute Donation d, @RequestParam("doCategory") String category ) {
-		ArrayList<Donation> list = dService.donationlist(category);
+	public String donationlist(@ModelAttribute Donation d) {
 
 		
-		return list;
+		return "/page/donationlist";
 	}
+	
+    @PostMapping("/page/donationlist")
+    public String handleCategory(@RequestParam String category) {
+        return category;
+    }
+	
+	
 	
 	@GetMapping("/admin/donaList")
 	public String newsList(Model model) {
