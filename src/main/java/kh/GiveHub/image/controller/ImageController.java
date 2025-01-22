6c7a3d9769ae79b6edbd0eb5b997b73399746cda
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-01-22 21:45:48 +0900

    image controller, service, exception

--- src/main/java/kh/GiveHub/image/controller/ImageController.java
+++ src/main/java/kh/GiveHub/image/controller/ImageController.java
@@ -1,7 +1,16 @@
 package kh.GiveHub.image.controller;
 
+import java.io.File;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import kh.GiveHub.image.model.service.ImageService;
 import lombok.RequiredArgsConstructor;
@@ -12,5 +21,31 @@ import lombok.RequiredArgsConstructor;
 public class ImageController {
 	private final ImageService iService;
 	
+	@PostMapping("/temp")
+	@ResponseBody
+	public ResponseEntity<String> saveTemp(
+		@RequestParam("image") MultipartFile file,
+		@RequestParam("imgType") String imgType,
+		@RequestParam("imgName") String imgName) {
+		String tempname = iService.saveTemp(file, imgName, imgType);
+		
+		return ResponseEntity.ok("/temp/"+tempname);
+	}
+	
+	@DeleteMapping("/delete")
+	@ResponseBody
+	public boolean deleteTemp(
+			@RequestParam("tempFileNames") List<String> tempFileNames) {
+		int length = tempFileNames.size();
+		int i = 0;
+		for(String tempname : tempFileNames) {
+			File tempFile = new File("/temp/"+tempname);
+			if (tempFile.exists()) {
+				tempFile.delete();
+			}
+			i++;
+		}
+		return length==i? true:false;
+	}
 	
 }
