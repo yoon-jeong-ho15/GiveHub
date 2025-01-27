5fe2f1ea6a3abd0e95eaeac92b0827579262b835
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-01-27 17:09:48 +0900

    .

--- src/main/resources/static/js/donationWrite.js
+++ src/main/resources/static/js/donationWrite.js
@@ -32,6 +32,7 @@ fakeGoal.addEventListener("input", function () {
 /////게시글 작성 관련
 const pathArr =[];
 //api
+tinymce.getContent("doContent").getBody().innerHTML = /*"[[${d.doContent}]]"*/""; 
 tinymce.init({
     license_key: "gpl",
     selector: "#doContent",
