53e0ababb0876e6d2b41dd31ca66e748df229c5c
shpark47 <shp04078@naver.com>
2025-01-31 10:06:59 +0900

    결제 정상화

--- src/main/java/kh/GiveHub/payment/PaymentController.java
+++ src/main/java/kh/GiveHub/payment/PaymentController.java
@@ -1,19 +1,18 @@
 package kh.GiveHub.payment;
 
+import kh.GiveHub.payment.model.service.PaymentService;
+import kh.GiveHub.payment.model.vo.PaymentRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import kh.GiveHub.member.model.exception.MemberException;
-//import kh.GiveHub.payment.model.service.PaymentService;
 import kh.GiveHub.payment.model.vo.Payment;
-//import kh.GiveHub.payment.model.vo.PaymentRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-//@RequestMapping("/pay")
 public class PaymentController {
 	
 	private final PaymentService pService;
@@ -44,9 +43,5 @@ public class PaymentController {
     	}else {
     		throw new MemberException("결제 실패");
     	}
-
-        // 여기에서 결제 정보를 서버에서 처리하고 저장할 수 있습니다.
-        // 예를 들어 결제 완료 상태를 DB에 저장하거나, 알림을 보낼 수 있습니다.
-
     }
 }
