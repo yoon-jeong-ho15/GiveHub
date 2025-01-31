e3af07a71dfa1345341ade4c0286fa34c80e5079
shpark <shp04078@naver.com>
2025-01-31 10:05:43 +0900

    Update PaymentController.java

--- src/main/java/kh/GiveHub/payment/PaymentController.java
+++ src/main/java/kh/GiveHub/payment/PaymentController.java
@@ -16,29 +16,8 @@ import lombok.RequiredArgsConstructor;
 //@RequestMapping("/pay")
 public class PaymentController {
 	
-//	private final PaymentService pService;
+	private final PaymentService pService;
 	
-//    @PostMapping
-//    public ResponseEntity<Map<String, String>> handlePayment(@RequestParam("amount") String amount,
-//                                                              @RequestParam(value = "customAmount", required = false) String customAmount,
-//                                                              HttpServletRequest request) {
-//        // 사용자로부터 전달된 기부 금액 처리
-//        String selectedAmount = amount;
-//        if (customAmount != null && !customAmount.isEmpty()) {
-//            selectedAmount = customAmount;
-//        }
-//
-//        // 결제 정보 확인
-//        Map<String, String> response = new HashMap<>();
-//        response.put("amount", selectedAmount);
-//        response.put("message", "기부 금액이 정상적으로 처리되었습니다.");
-//
-//        // 여기서 아임포트 결제 API 호출 등을 처리할 수 있습니다.
-//
-//        return ResponseEntity.ok(response);
-//    }
-
-<<<<<<< HEAD
     @PostMapping("/payment/complete")
     public ResponseEntity<String> paymentComplete(@RequestBody PaymentRequest request) {
         // 결제 완료 후 처리
@@ -70,36 +49,4 @@ public class PaymentController {
         // 예를 들어 결제 완료 상태를 DB에 저장하거나, 알림을 보낼 수 있습니다.
 
     }
-=======
-//    @PostMapping("/payment/complete")
-//    public ResponseEntity<String> paymentComplete(@RequestBody PaymentRequest request) {
-//        // 결제 완료 후 처리
-//    	Payment payment = new Payment();
-//
-//    	System.out.println(request.getAmount());
-//    	System.out.println(request.getName());
-//    	System.out.println(request.getDoNo());
-//
-//    	payment.setAmount(request.getAmount());
-//    	payment.setDonorName(request.getName());
-//    	payment.setDonationNo(request.getDoNo());
-//    	payment.
-//    	payment.setSuccess(true);
-//
-//    	int result = pService.savePayment(request);
-//
-//    	System.out.println(result);
-//
-//    	if(result>0) {
-//    		return ResponseEntity.ok("결제 완료");
-//
-//    	}else {
-//    		throw new MemberException("결제 실패");
-//    	}
-//
-//        // 여기에서 결제 정보를 서버에서 처리하고 저장할 수 있습니다.
-//        // 예를 들어 결제 완료 상태를 DB에 저장하거나, 알림을 보낼 수 있습니다.
-//
-//    }
->>>>>>> branch 'main' of https://github.com/shpark47/GiveHub.git
 }
