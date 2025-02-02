d9a84c4c92a31360f2500f860c08632eda7abcb2
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-02-02 18:20:57 +0900

    수정할 때 이미지 upload폴더랑 db에서 삭제

--- src/main/java/kh/GiveHub/image/model/service/ImageService.java
+++ src/main/java/kh/GiveHub/image/model/service/ImageService.java
@@ -105,19 +105,32 @@ public class ImageService {
 	}
 
 	public boolean deleteImage(List<String> delFiles) {
-		int delcount = 0;
-        for (String filename : delFiles) {
-        	File file = new File(uploadPath+filename);
-        	if(file.exists()) {
-        		file.delete();
-        	}
-        	delcount += mapper.deleteImage(filename);
-        }
-        System.out.println("delCount : "+delcount);
-        System.out.println("delFiles.size() : "+delFiles.size());
-        if (delcount == delFiles.size()) {
-        	return true;
-        }
-		return false;
+	    int totalCount = delFiles.size();
+	    int successCount = 0;
+	    
+	    for (String filename : delFiles) {
+	        File file = new File(uploadPath + filename);
+	        boolean fileDeleted = false;
+	        
+	        if (file.exists()) {
+	            try {
+	                fileDeleted = file.delete();
+	                if (fileDeleted) {
+	                    int dbResult = mapper.deleteImage(filename);
+	                    if (dbResult > 0) {
+	                        successCount++;
+	                    }
+	                }
+	            } catch (SecurityException e) {
+	                e.printStackTrace();
+	            }
+	        } else {
+	            int dbResult = mapper.deleteImage(filename);
+	            if (dbResult > 0) {
+	                successCount++;
+	            }
+	        }
+	    }
+	    return successCount == totalCount;
 	}
 }
